2612ec112a369985deca2f8b25c09b9ba9cf6c99
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-04-22 18:36:02 +0300

    Update Specifications.java

--- src/test/java/org/ibs/Specifications.java
+++ src/test/java/org/ibs/Specifications.java
@@ -5,13 +5,15 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-public class Specifications {
-//    public static SSLConfig config = new SSLConfig().keyStore("", "").trustStore("", "");
-//    static RestAssuredConfig config1 = RestAssuredConfig.newConfig().sslConfig(config);
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
 
+public class Specifications {
+    static String sessionID;
 
     public static RequestSpecification requestSpecification(String url) {
         return new RequestSpecBuilder()
@@ -28,5 +30,28 @@ public class Specifications {
     public static void installSpecification(RequestSpecification requestSpecification, ResponseSpecification responseSpecification) {
         RestAssured.requestSpecification = requestSpecification;
         RestAssured.responseSpecification = responseSpecification;
+    }
+
+    public static void getInfo (String foodName) {
+        given()
+                .header ("Accept", "application/json")
+                .header ("Content-Type", "application/json")
+                .cookie ("JSESSIONID", sessionID)
+                .when ()
+                .get ("/food")
+                .then ()
+                .log ().all ()
+                .assertThat ()
+                .body ("name", hasItem(foodName));
+    }
+
+    public static Response getFood () {
+        Response response = given ()
+                .header ("Accept", "application/json")
+                .header ("Content-Type", "application/json")
+                .when ()
+                .get ("/food");
+        sessionID = response.getCookie ("JSESSIONID");
+        return response;
     }
 }
